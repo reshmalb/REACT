0c9f8cb65607b52c63d08456a33f8af9d90a0c0f
reshmalb <reshma.lb27@gmail.com>
2023-03-12 16:01:33 +0530

    Delete button event listener added

--- expenseapp/src/App.js
+++ expenseapp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 function App() { 
-  const details=[
+    const details=[
     {  date:new Date(2021,1,10),
      title:'vacation',
     amount:200
@@ -16,6 +16,7 @@ function App() {
 
   return (
     <div className="App">
+     <div className="expense-item-container">
      <ExpenseItem date={details[0].date} 
                  title={details[0].title}
                  amount={details[0].amount} ></ExpenseItem>
@@ -23,6 +24,8 @@ function App() {
                  title={details[1].title}
                  amount={details[1].amount} ></ExpenseItem>
     </div>
+    </div>
+ 
   );
 }
 
--- expenseapp/src/components/ExpenseItem.js
+++ expenseapp/src/components/ExpenseItem.js
@@ -3,12 +3,16 @@ import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css'
 const ExpenseItem=(props)=>{
     return(
-        <div className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item_description"/>
-                <h2>{props.title}</h2>
-            <div className="expense-item_price">${props.amount}</div>
-        </div>
+    
+            <div  className="div-subdata">
+                <ExpenseDate date={props.date}/>
+                 <div className="expense-item_description">       
+                    {props.title}</div>
+                 <div className="expense-item_price">${props.amount}
+                 </div>
+          </div>
+    
+    
 
     );
 }
