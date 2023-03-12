0871703e3c161ef8decd319c8c240efec5b9ad59
reshmalb <reshma.lb27@gmail.com>
2023-03-12 16:27:03 +0530

    event handler added

--- expenseapp/src/components/ExpenseItem.js
+++ expenseapp/src/components/ExpenseItem.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css'
 const ExpenseItem=(props)=>{
+    const clickHandler=()=>{
+        console.log("button clicked!!!")
+    }
     return(
-    
-            <div  className="div-subdata">
+     <div className='expense-item'>
+            <div  className='div-subdata'>
                 <ExpenseDate date={props.date}/>
                  <div className="expense-item_description">       
                     {props.title}</div>
                  <div className="expense-item_price">${props.amount}
+                 <button  className="button-delete"
+                 onClick={clickHandler}>Delete Expense</button>
                  </div>
           </div>
-    
+     </div>
     
 
     );
