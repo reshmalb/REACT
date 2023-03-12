199994de3be2a6384163646537d2285beebeb6e6
reshmalb <reshma.lb27@gmail.com>
2023-03-12 23:32:55 +0530

    change amount button added

--- expenseapp/src/components/ExpenseItem.js
+++ expenseapp/src/components/ExpenseItem.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css'
 const ExpenseItem=(props)=>{
+   const [title,setTitle]= useState(props.title);
+   const [amount,setAmount]=useState(props.amount)
+
     const clickHandler=()=>{
-        console.log("button clicked!!!")
+        setAmount(100);
+        console.log(amount)
     }
     return(
      <div className='expense-item'>
             <div  className='div-subdata'>
                 <ExpenseDate date={props.date}/>
                  <div className="expense-item_description">       
-                    {props.title}</div>
-                 <div className="expense-item_price">${props.amount}
-                 <button  className="button-delete"
-                 onClick={clickHandler}>Delete Expense</button>
+                    {title}</div>
+                 <div className="expense-item_price">${amount}
+                <button  className="button-delete"
+                 onClick={clickHandler}>change amount</button>
+                 
                  </div>
           </div>
      </div>
