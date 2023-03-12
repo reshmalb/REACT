1434a823d6543754cfdc41e1cd9ad7629f3e7be7
reshmalb <reshma.lb27@gmail.com>
2023-03-12 15:01:09 +0530

    date component added

--- expenseapp/src/index.js
+++ expenseapp/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
+  <header><h1 id="section">Expense Data</h1></header>    
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
