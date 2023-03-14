b5428f1278b70005f27be446803922c59ea933c1
reshmalb <reshma.lb27@gmail.com>
2023-03-14 10:09:06 +0530

    dynamic display

--- expenseapp/src/components/Expenses.js
+++ expenseapp/src/components/Expenses.js
@@ -6,15 +6,16 @@ import Card from "./Card"
 
 
 const Expenses=(props)=>{
+   
     return(
     <div >
-    <ExpenseItem date={props.items[0].date} 
-                title={props.items[0].title}
-                amount={props.items[0].amount} ></ExpenseItem>
-   <ExpenseItem date={props.items[1].date} 
-                title={props.items[0].title}
-                amount={props.items[0].amount} ></ExpenseItem>
-   </div>
-    )
+    
+      <ExpenseItem date={props.items.date} 
+                title={props.items.title}
+                amount={props.items.amount} ></ExpenseItem>
+    
+    
+   </div>)
+      
 }
 export default Expenses;
